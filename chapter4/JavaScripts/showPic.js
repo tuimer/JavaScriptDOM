93e247be4fbcdd97cca3075dd4ff2c2548197c2a
tuimer <1296653792@qq.com>
2018-02-04 00:12:09 +0800

    增加countBodyChildren函数

--- chapter4/JavaScripts/showPic.js
+++ chapter4/JavaScripts/showPic.js
@@ -1,5 +1,12 @@
 function showPic(whichPic){
 	 var source=whichPic.getAttribute("href");
 	 var placeholder=document.getElementById("placeholder");
-	 placeholder.setAttribute("src",source);
+	 placeholder.setAttribute("src",source); 
 }
+function countBodyChildren(){
+	var body_element=document.getElementsByTagName("body")[0];
+	alert(body_element.childNodes.length);
+}
+window.onload=countBodyChildren;
+	
+
