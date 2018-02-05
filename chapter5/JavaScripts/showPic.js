040b2e80fba7418ce5f216bfe45ed760a84ecdc5
tuimer <1296653792@qq.com>
2018-02-05 17:30:05 +0800

    第五章

---
+++ chapter5/JavaScripts/showPic.js
@@ -0,0 +1,29 @@
+function showPic(whichPic){
+	 var source=whichPic.getAttribute("href");
+	 var placeholder=document.getElementById("placeholder");
+	 placeholder.setAttribute("src",source); //设置图片变换
+	 var text=whichPic.getAttribute("title");//点击图片时，链接的title属性值提取并保留到text
+	 var description=document.getElementById("description");//id为description的<p>元素保留在description的变量里
+	 description.firstChild.nodeValue=text;//firstChild等价于childNodes[0]  
+}
+function countBodyChildren(){
+	var body_element=document.getElementsByTagName("body")[0];
+	alert(body_element.nodeType);//节点的nodeType属性值：1——>元素节点
+}								 //						 2——>属性节点
+window.onload=countBodyChildren; //						 3——>文本节点
+function popUp(winURL){
+	window.open(winURL,"popup","width=320,height=480");
+}	
+window.onload=function(){
+	if(!document.getElementByTagName) return false;
+	var lnk=document.getElementByTagName("a");
+	for(var i=0;i<lnk.length;i++){
+		if(lnk[i].getAttribute("class")=="popup"){
+			lnk[i].onclick=function(){
+				popUp(this.getAttribute("href"));
+				return false;
+			}
+		}
+	}
+}
+
