ecb5f492110d38d8dd5e4e09012e6cca4d4244fe
tuimer <1296653792@qq.com>
2018-02-05 12:44:07 +0800

    添加第五章弹窗函数

--- chapter4/JavaScripts/showPic.js
+++ chapter4/JavaScripts/showPic.js
@@ -4,12 +4,14 @@ function showPic(whichPic){
 	 placeholder.setAttribute("src",source); //设置图片变换
 	 var text=whichPic.getAttribute("title");//点击图片时，链接的title属性值提取并保留到text
 	 var description=document.getElementById("description");//id为description的<p>元素保留在description的变量里
-	 description.firstChild.nodeValue=text;//firstChild等价于childNodes[0]
+	 description.firstChild.nodeValue=text;//firstChild等价于childNodes[0]  
 }
 function countBodyChildren(){
 	var body_element=document.getElementsByTagName("body")[0];
 	alert(body_element.nodeType);//节点的nodeType属性值：1——>元素节点
 }								 //						 2——>属性节点
 window.onload=countBodyChildren; //						 3——>文本节点
-	
+function popUp(winURL){
+	window.open(winURL,"popup","width=320,height=480");
+}	
 
